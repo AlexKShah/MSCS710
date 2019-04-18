a304168c35d2bff1a0c254316bc2a7e1c112df5b
StevenGuarino <guarinosteven50@gmail.com>
2019-04-18 11:17:14 -0400

    Sys poll (#4)
    * sys poll writing to db almost complete
    
    * insert working
    
    * delete working

---
+++ flask/flask/db_utils.py
@@ -0,0 +1,121 @@
+import mysql.connector as mariadb
+
+__author__ = "StevenGuarino"
+__version__ = "0.1"
+
+class database_obj():
+  def __init__(self,
+               user: str,
+               password: str,
+               database: str,
+               keep_existing: bool):
+      self.user = user
+      self.password = password
+      self.database = database
+      self._mk_connection()
+      # case where not using existing and something is there
+      if not keep_existing and self.check_db_exists(self.database):
+        self._drop_db(self.database)
+        self._create_db()
+      # case where nothing is there
+      elif not self.check_db_exists(self.database):
+        self._create_db()
+      # case where something is there and we are keeping 
+      elif keep_existing and self.check_db_exists(self.database):
+        self.mariadb_connection = mariadb.connect(user=self.user,
+                                                  passwd=self.password,
+                                                  database=self.database)
+  # end
+
+  def _mk_connection(self):
+    self.mariadb_connection = mariadb.connect(user=self.user,
+                                              passwd=self.password)
+    self.cursor = self.mariadb_connection.cursor()
+  # end
+ 
+  def _create_db(self):
+    self.cursor.execute("CREATE DATABASE {}".format(self.database))
+    self.mariadb_connection = mariadb.connect(user=self.user,
+                                              passwd=self.password,
+                                              database=self.database)
+    self.cursor = self.mariadb_connection.cursor()
+  # end
+
+  def create_table(self,
+                   table_name: str,
+                   cols: str):
+    cols =  "id int NOT NULL AUTO_INCREMENT, "+cols+", PRIMARY KEY (id)" 
+    self.cursor.execute("CREATE TABLE {} ({})".format(table_name,
+                                                      cols))
+  # end
+
+  def check_db_exists(self,
+                      db_name: str) -> bool:
+    check = "SHOW DATABASES"
+    self.cursor.execute(check)
+    found = False
+    for database in self.cursor:
+      if database[0] == db_name:
+          found = True
+    return found
+  # end
+
+  def check_table_exists(self,
+                         table_name: str) -> bool:
+    check = "SHOW TABLES"
+    self.cursor.execute(check)
+
+    found = False
+    for table in self.cursor:
+      if table[0] == table_name:
+          found = True
+    return found
+  # end
+
+  def insert_into_table(self,
+                        table_name: str,
+                        cols: list,
+                        vals: list):
+    sql = "INSERT INTO {} ({}) VALUES ({})".format(table_name,
+                                                   cols,
+                                                   str("%s, "*len(vals[0]))[:-2])
+    self.cursor.executemany(sql, vals)
+    self.mariadb_connection.commit()
+    print(self.cursor.rowcount, "were inserted")
+  # end
+
+  def delete_from_table(self,
+                        table_name: str,
+                        delete_interval: int,
+                        date_col: str):
+    sql = "DELETE FROM {} WHERE {} < (NOW() - INTERVAL {} MINUTE)".format(table_name,
+                                                                         date_col,
+                                                                         delete_interval)
+    self.cursor.execute(sql)
+  # end
+
+  def select_from_table(self,
+                        table_name: str,
+                        cols: list):
+    self.cursor.execute("SELECT {} FROM {}".format(", ".join(cols), table_name))
+    return self.cursor.fetchall()
+  # end
+
+  def _drop_db(self,
+              db_name: str):
+    use_statement = "USE {}".format(db_name)
+    show_tables_statement = "SHOW TABLES"
+    drop_db_statement = "DROP DATABASE {}".format(db_name)
+    self.cursor.execute(use_statement)
+    self.cursor.execute(show_tables_statement)
+    for table in self.cursor:
+      self._drop_table(table[0])
+    self.cursor.execute(drop_db_statement)
+  # end
+
+  def _drop_table(self,
+                 table_name: str):
+    drop_tables_statement = "DROP TABLES {}".format(table_name)
+    self.cursor.execute(drop_tables_statement)
+  # end
+# end
--- flask/flask/sys_poll.py
+++ flask/flask/sys_poll.py
@@ -10,15 +10,19 @@ import traceback
 from timeit import default_timer as timer
 import datetime
 from itertools import repeat
+import schedule
+import time
+
+from db_utils import database_obj
 
 __author__ = "StevenGuarino"
 __version__ = "0.1"
 
 """
 TODOs:
-  currently not using --time_zone
-  not putting items in database yet
-  add error handling
+  * currently not using --time_zone
+  * add error handling
+  * parallel processing not working on alex server
 """
 
 def get_args():
@@ -28,6 +32,10 @@ def get_args():
   args: dictionary of cli arguments
   """
   parser = argparse.ArgumentParser(description="this script pull sys information")
+  parser.add_argument("--poll_every",
+                      help="how often (seconds) to poll system information",
+                      type=int,
+                      default=10)
   parser.add_argument("--time_zone",
                       help="specify the time zone of the machine to gather sys information from",
                       type=str,
@@ -39,116 +47,177 @@ def get_args():
   parser.add_argument("--process_in_parallel",
                       help="use parallel processing where appropiate",
                       type=bool,
-                      default=True)
+                      default=False)
+  parser.add_argument("--poll_db",
+                      help="database to write recently polled data",
+                      type=str,
+                      default="poll")
+  parser.add_argument("--keep_existing",
+                      help="keep existing database",
+                      type=bool,
+                      default=False)
+  parser.add_argument("--db_username",
+                      help="database user name",
+                      type=str,
+                      default="root")
+  parser.add_argument("--db_password",
+                      help="database password",
+                      type=str,
+                      default="alex")
+  parser.add_argument("--delete_interval",
+                      help="how often to remove records (mintues)",
+                      type=int,
+                      default=60)
   args = parser.parse_args()
   return args
 # end
 
-def get_processes():
-  """
-  desc: fetch process ids
-  returns: pids for all running processes
-  """
-  return [pid for pid in os.listdir("/proc") if pid.isdigit()]
-# end
+class sys_poll():
+  def __init__(self,
+               cli_args):
+    self.cli_args = cli_args
+    self.table_names = "current"
+    self.delete_interval = self.cli_args["delete_interval"]
+    self.poll_db = database_obj(user=self.cli_args["db_username"],
+                                password=self.cli_args["db_password"],
+                                database=self.cli_args["poll_db"],
+                                keep_existing=self.cli_args["keep_existing"])
+  # end
 
-def get_process_metrics(args):
-  """
-  desc: get metrics for a given process id
-  args:
-    unpacks to:
-      pid: process id (str)
-      metrics: metrics to pull
-  returns: dict of metrics for a process id
-  """
-  pid, metrics = args
-  p_metrics = {metric: getattr(pid, metric)() for metric in metrics if hasattr(pid, metric)} 
-  p_metrics["current_time"] = str(datetime.datetime.now())
-  p_metrics["pid"] = pid.pid
-  return p_metrics
-# end
+  def get_processes(self):
+    """
+    desc: fetch process ids
+    returns: pids for all running processes
+    """
+    return [pid for pid in os.listdir("/proc") if pid.isdigit()]
+  # end
 
-def worker_init(queue):
-  """
-  desc: init for worker for logging during multiprocessing
-  args: 
-    queue: queue for logging
-  """
-  # all records from worker processes go to qh and then into q
-  queue_handler = QueueHandler(queue)
-  logger = logging.getLogger()
-  logger.setLevel(logging.DEBUG)
-  logger.addHandler(queue_handler)
-# end
+  def get_process_metrics(self,
+                          args):
+    """
+    desc: get metrics for a given process id
+    args:
+      unpacks to:
+        pid: process id (str)
+        metrics: metrics to pull
+    returns: dict of metrics for a process id
+    """
+    pid, metrics = args
+    p_metrics = {metric: getattr(pid, metric)() for metric in metrics if hasattr(pid, metric)} 
+    p_metrics["nowtime"] = str(datetime.datetime.now())
+    p_metrics["pid"] = pid.pid
+    return p_metrics
+  # end
 
-def logger_init(path, filename):
-  """
-  desc: create logger
-  args:
-    path: path to logging directory 
-    filename: log filename 
-  returns:
-    logger
-    queue_listener
-    queue
-  """
-  queue = Queue()
-  # handler for all log records
-  handler = logging.StreamHandler()
-  handler.setFormatter(logging.Formatter("%(levelname)s: %(asctime)s - %(process)s - %(message)s"))
+  def worker_init(self,
+                  queue):
+    """
+    desc: init for worker for logging during multiprocessing
+    args: 
+      queue: queue for logging
+    """
+    # all records from worker processes go to qh and then into q
+    queue_handler = QueueHandler(queue)
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(queue_handler)
+  # end
 
-  # queue_listener gets records from the queue and sends them to the handler
-  queue_listener = QueueListener(queue, handler)
-  queue_listener.start()
-
-  # currentTime = str(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
-  logFileName = os.path.join(path, "{}.log".format(filename))
-  logging.basicConfig(level=logging.INFO,
-                      format='%(asctime)s %(levelname)s %(message)s',
-                      filename=logFileName,
-                      filemode='w')
-
-  logger = logging.getLogger()
-
-  logger.setLevel(logging.INFO)
-  # add the handler to the logger so records from this process are handled
-  logger.addHandler(handler)
-
-  return logger, queue_listener, queue
-# end
-
-def sys_poll_main():
-  """
-  desc: get system information by process
-  returns: pandas dataframe with system metrics by process id
-  """
-  args = get_args()
-  logger, queue_listener, queue = logger_init(path=os.path.join(os.getcwd(), args.log_dir),
-                                              filename="sys_poll")
-  """
-  note: to add more metrics simply go through https://psutil.readthedocs.io/en/latest/
-        find methods on Process object and add to `metrics` list below
-  """
-  metrics = ["memory_percent",
-             "cpu_percent",
-             "num_threads",
-             "name"] # list of metrics to call on psutil.Process object
-  pids = list(map(int, get_processes()))
-  process_objs = [psutil.Process(pid) for pid in pids]
+  def logger_init(self,
+                  path,
+                  filename):
+    """
+    desc: create logger
+    args:
+      path: path to logging directory 
+      filename: log filename 
+    returns:
+      logger
+      queue_listener
+      queue
+    """
+    queue = Queue()
+    # handler for all log records
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("%(levelname)s: %(asctime)s - %(process)s - %(message)s"))
   
-  if args.process_in_parallel:
-    pool = Pool(os.cpu_count(), worker_init, [queue])
-    all_process_metrics = [process_metrics for process_metrics in pool.map(get_process_metrics, list(zip(process_objs, repeat(metrics, len(process_objs)))))]
-    pool.close()
-    pool.join()
-    queue_listener.stop()
-  else:
-    all_process_metrics = [get_process_metrics([process_objs, metrics_])
-                            for process_objs, metrics_ in list(zip(process_objs, repeat(metrics, len(process_objs))))]
-  all_process_metrics = pd.DataFrame(all_process_metrics)
-  return all_process_metrics
+    # queue_listener gets records from the queue and sends them to the handler
+    queue_listener = QueueListener(queue, handler)
+    queue_listener.start()
+  
+    # currentTime = str(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
+    if not os.path.exists(path):
+      os.mkdir(path)
+    logFileName = os.path.join(path, "{}.log".format(filename))
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s %(levelname)s %(message)s',
+                        filename=logFileName,
+                        filemode='w')
+  
+    logger = logging.getLogger()
+  
+    logger.setLevel(logging.INFO)
+    # add the handler to the logger so records from this process are handled
+    logger.addHandler(handler)
+  
+    return logger, queue_listener, queue
+  # end
 
+  def main(self):
+    """
+    desc: get system information by process
+    returns: pandas dataframe with system metrics by process id
+    """
+    logger, queue_listener, queue = self.logger_init(os.path.join(os.getcwd(),
+                                                                  self.cli_args["log_dir"]),
+                                                     filename="sys_poll")
+    """
+    note: to add more metrics simply go through https://psutil.readthedocs.io/en/latest/
+          find methods on Process object and add to `metrics` list below
+    """
+    metrics = ["memory_percent",
+               "cpu_percent",
+               "num_threads",
+               "name"] # list of metrics to call on psutil.Process object
+    pids = list(map(int, self.get_processes()))
+    process_objs = [psutil.Process(pid) for pid in pids]
+
+    if self.cli_args["process_in_parallel"]:
+      pool = Pool(os.cpu_count(), self.worker_init, [queue])
+      all_process_metrics = [process_metrics for process_metrics in pool.map(self.get_process_metrics, list(zip(process_objs, repeat(metrics, len(process_objs)))))]
+      pool.close()
+      pool.join()
+      queue_listener.stop()
+    else:
+      all_process_metrics = [self.get_process_metrics([process_objs, metrics_])
+                              for process_objs, metrics_ in list(zip(process_objs, repeat(metrics, len(process_objs))))]
+    all_process_metrics = pd.DataFrame(all_process_metrics)
+
+    if not self.poll_db.check_table_exists(self.table_names):
+      cols = [] # TODO add unique identifier
+      for key in all_process_metrics.keys():
+        if key == "nowtime": cols.append(str(key) + " datetime")
+        else: cols.append(str(key) + " varchar(255)")
+      cols = ", ".join(cols)
+      self.poll_db.create_table(self.table_names, cols) # create current table
+
+    keys = list(all_process_metrics.keys())
+    vals = list(zip(*[all_process_metrics[k].values.tolist() for k in keys]))
+    self.poll_db.insert_into_table(self.table_names,
+                                   ", ".join(keys),
+                                   vals)
+    self.poll_db.delete_from_table(self.table_names,
+                                   "nowtime",
+                                   self.delete_interval)
+  # end
 # end
 
 if __name__ == "__main__":
-  all_process_metrics = sys_poll_main()
+  args = get_args()
+  sys_poll_obj = sys_poll(vars(args))
+
+  schedule.every(args.poll_every).seconds.do(sys_poll_obj.main)
+
+  while True:
+    schedule.run_pending()
+    time.sleep(1)
