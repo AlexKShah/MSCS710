5634facc6ba54c296e4581254b81f27951493961
alexkshah <alexkshah@gmail.com>
2019-04-18 16:26:32 -0400

    fixed testing automation and logs

--- flask/flask/tests.sh
+++ flask/flask/tests.sh
@@ -2,6 +2,9 @@
 
 #execute test files, write logs
 exec python3 tests.py 2&>1 | tee -a /tmp/tests.log;
+echo "py done" | tee -a /tmp/tests.log;
 sleep 5;
+
 #read logs
 cat /tmp/tests.log;
+echo "bash done";
