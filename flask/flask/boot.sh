56b15f02ca08c1582871e7aca0f8cdc281c9b9b4
Alexander Shah <AlexKShah@users.noreply.github.com>
2019-04-24 11:42:30 -0400

    modify gunicorn flask launcher

--- flask/flask/boot.sh
+++ flask/flask/boot.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-exec gunicorn -b :5000 --access-logfile - --error-logfile - run:app
+exec export FLASK_APP=run.py
+exec gunicorn -w 4 -b 0.0.0.0:5000 "run:create_app()"
--- flask/flask/run.py
+++ flask/flask/run.py
@@ -1,4 +1,5 @@
 #!flask/bin/python
-from app import app
+from app import app, create_app
+app = create_app()
 app.debug = True
-app.run(host = '0.0.0.0',port=5000)
+#app.run(host = '0.0.0.0',port=5000)
