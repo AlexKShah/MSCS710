b833a8630947affa53f8da70f39d710e46459292
alexkshah <alexander.shah2@marist.edu>
2019-04-25 15:19:16 -0400

    fix default model datetime

--- limon/flask/app/models.py
+++ limon/flask/app/models.py
@@ -1,10 +1,11 @@
 from app import db
+import datetime
 
 class Metric(db.Model):
     cpu_percent = db.Column(db.String(255))
     memory_percent = db.Column(db.String(255))
     name = db.Column(db.String(255))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime, index=True, default=str(datetime.datetime.now()))
     num_threads = db.Column(db.String(255))
     pid = db.Column(db.Integer, primary_key=True)
 
