0bc94c409a2589b365a58123041b7056618b8060
alexkshah <alexander.shah2@marist.edu>
2019-04-25 16:08:49 -0400

    flask running on boot up

--- limon/flask/app/__init__.py
+++ limon/flask/app/__init__.py
@@ -4,25 +4,13 @@ import os
 import yaml
 import pathlib
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# get database parameters from config
+#get database parameters from config
 with open("sys_poll.yml", 'r') as configfile:
     cfg = yaml.safe_load(configfile)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://' + cfg['db_username'] + ':'+ cfg['db_password'] + '@' + cfg['db_host'] + '/poll'
 db = SQLAlchemy(app)
-
-@app.route('/bower_components/<path:path>')
-def send_bower(path):
-    return send_from_directory(os.path.join(app.root_path, 'bower_components'), path)
-
-@app.route('/dist/<path:path>')
-def send_dist(path):
-    return send_from_directory(os.path.join(app.root_path, 'dist'), path)
-
-@app.route('/js/<path:path>')
-def send_js(path):
-    return send_from_directory(os.path.join(app.root_path, 'js'), path)
 
 from app import routes, models
